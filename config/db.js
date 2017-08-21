32691d7ba019dd0dd548671797e604fe738c0424
Jinyoung <jpa162@uclive.ac.nz>
2017-08-21 23:19:58 +1200

    UPDATE

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
+/*
 const mysql = require('mysql');
 const importer = require('node-mysql-importer');
+const fs = require('fs');
 
 const state = {
     pool: null
@@ -20,15 +22,61 @@ exports.connect = function(done){
 };
 
 exports.createTables = function(){
-  importer.config(config);
+  var sql = fs.readFileSync("./table.sql").toString();
+    state.pool.query(sql, function(err, rows) {
+        if(!err){
 
-  importer.importSQL('tabel.sql').then( () => {
-    console.log('statement executed');
-  }).catch( err=> {
-    console.log('err');
-  })
+        } else {
+
+        }
+    })
+
+
+  // importer.importSQL('table.sql').then( () => {
+  //   console.log('statement executed');
+  // }).catch( err => {
+  //   console.log('err');
+  // })
 }
 
 exports.get = function(){
     return state.pool;
+}
+*/
+
+const mysql = require('mysql');
+const fs = require('fs');
+
+
+const state = {
+    pool: null
+};
+
+exports.connect = function(done){
+    state.pool = mysql.createPool({
+        multipleStatements: true,
+        host: process.env.SENG365_MYSQL_HOST || 'localhost',
+        port: process.env.SENG365_MYSQL_PORT || 6033,
+        user: 'root',
+        password: 'secret',
+        database: 'mysql'
+        // database: 'mysql'
+    });
+    done();
+};
+
+exports.get = function(){
+    return state.pool;
+}
+
+exports.DB = function(done){
+    var sql = fs.readFileSync("./table.sql").toString();
+    state.pool.query(sql, function(err, rows){
+        if (!err){
+            console.log("db create");
+        } else {
+            console.log(err);
+        }
+    });
+    done();
 }
